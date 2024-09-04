763897ed5e4e85225dbe6a283b6cb28b3c34dd79
Nataliya_Yavtushenko <yavtushenkon@gmail.com>
2024-09-04 21:03:38 +0300

    mob3

--- js/task-1.js
+++ js/task-1.js
@@ -6,9 +6,3 @@ console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-for (const key in object) {
-  if (Object.hasOwnProperty.call(object, key)) {
-    const element = object[key];
-  }
-}
